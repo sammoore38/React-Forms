bd74c8ea62b632612df2cad41ba7c8b0d5242408
sammoore38 <127150634+sammoore38@users.noreply.github.com>
2023-08-14 16:24:36 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function SignUpForm({ setToken }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log("Signup Result: ", result);
+      setToken(result.token);
+      setSuccessMessage(result.message);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
